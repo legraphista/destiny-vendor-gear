437333602f3911c54ed938c203d20de0f36d9205
Ștefan-Gabriel Muscalu <legraphista@users.noreply.github.com>
2021-09-23 20:38:08 +0300

    update: config tokens
    i'll fix and update those tomorrow

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,2 +1,2 @@
-export const API_TOKEN: string = process.env.API_TOKEN as unknown as string;
-export const OAUTH_CLIENT_ID: string = process.env.OAUTH_CLIENT_ID as unknown as string;
+export const API_TOKEN: string = 'b719fc26c3d9485f8547d1498e0032a1';
+export const OAUTH_CLIENT_ID: string = '37792';
