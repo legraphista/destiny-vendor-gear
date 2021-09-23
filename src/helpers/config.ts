1981b95e6d068800e1bcfb246490be37170494d9
Ștefan-Gabriel Muscalu <legraphista@users.noreply.github.com>
2021-09-23 19:56:10 +0300

    update: attempt to fix process.env.* tokens

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,2 +1,2 @@
-export const API_TOKEN: string = process.env.API_TOKEN!;
-export const OAUTH_CLIENT_ID: string = process.env.OAUTH_CLIENT_ID!;
+export const API_TOKEN: string = process.env.API_TOKEN as unknown as string;
+export const OAUTH_CLIENT_ID: string = process.env.OAUTH_CLIENT_ID as unknown as string;
